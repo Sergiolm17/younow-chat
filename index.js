533f73417ca8f1537d5729cc47e22ee263d81e48
Sergiolm17 <kingsergiolm17@gmail.com>
2021-04-21 18:47:13 -0500

    add path

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const puppeteer = require("puppeteer");
 const express = require("express");
+const path = require("path");
+
 const socketio = require("socket.io");
 const app = express();
 const PORT = process.env.PORT || 3000;
